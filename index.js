5e9978ee3bbc1e43d6e23efd19cfda876928c633
Hai Trieu Ngoc <trieungochai.dev@gmail.com>
2022-05-28 12:17:50 +0700

    Finding habitable planets

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 const { parse } = require("csv-parse");
 const fs = require("fs");
 
-const results = [];
+const isHabitablePlanet = (planet) => {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+};
+
+const habitablePlanets = [];
 
 fs.createReadStream("kepler_data.csv")
   .pipe(
@@ -11,12 +20,13 @@ fs.createReadStream("kepler_data.csv")
     })
   )
   .on("data", (data) => {
-    results.push(data);
+    if (isHabitablePlanet(data)) {
+      habitablePlanets.push(data);
+    }
   })
   .on("error", (err) => {
     console.log(err);
   })
   .on("end", () => {
-    console.log(results);
-    console.log("Done");
+    console.log(`${habitablePlanets.length} habitable planets found!`);
   });
